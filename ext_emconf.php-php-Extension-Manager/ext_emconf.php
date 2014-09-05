55e7bdb584f71c1125074f2463898d58d73e6274
Manuel Bergeron <manuelbergeron@mx2.ca>
2014-09-05 12:47:49 -0400

    Create ext_emconf.php

---
+++ ext_emconf.php-php-Extension-Manager/ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "formhandler".
+#
+# Auto generated 05-03-2013 13:32
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+        'title' => 'Libeo Override Formhandler',
+        'description' => 'The swiss army knife for all kinds of mailforms, completely new written using the MVC concept. Result: Flexibility, Flexibility, Flexibility  :-).',
+        'category' => 'plugin',
+        'shy' => 0,
+        'version' => '1.5.1',
+        'dependencies' => '',
+        'conflicts' => '',
+        'priority' => '',
+        'loadOrder' => '',
+        'module' => '',
+        'state' => 'stable',
+        'uploadfolder' => 0,
+        'createDirs' => '',
+        'modify_tables' => '',
+        'clearcacheonload' => 0,
+        'lockType' => '',
+        'author' => 'Libeo',
+        'author_email' => 'info@libeo.com',
+        'author_company' => 'Libeo',
+        'CGLcompliance' => '',
+        'CGLcompliance_note' => '',
+        'constraints' => array(
+                'depends' => array(
+                        'php' => '5.2.0-0.0.0',
+                        'typo3' => '4.2.0-0.0.0',
+                ),
+                'conflicts' => array(
+                ),
+                'suggests' => array(
+                ),
+      ),
+);
+?>
